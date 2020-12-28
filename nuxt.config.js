111f12387d24701fe54fc221d3286743c5e40c39
Chris Vaillancourt <cvaillancourt@denovium.com>
2020-12-28 15:57:43 -0800

    chore(deps): remove vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
 const { CI_PAGES_URL } = process.env
 const base = CI_PAGES_URL && new URL(CI_PAGES_URL).pathname
 
@@ -40,8 +39,6 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
     '@nuxtjs/tailwindcss',
   ],
 
@@ -55,26 +52,6 @@ export default {
 
   // Content module configuration (https://go.nuxtjs.dev/config-content)
   content: {},
-
-  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
-  },
-
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,5 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">Welcome!</v-card-title>
-        <v-card-text>
-          <p>More soon...</p>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <p>Hello World</p>
+  </div>
 </template>
-
-<script>
-export default {}
-</script>
