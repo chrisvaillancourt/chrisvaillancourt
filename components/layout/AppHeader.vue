cbc576ba4a61eb649fd6327944380c7e979f3851
Chris Vaillancourt <cvaillancourt@denovium.com>
2020-12-29 14:37:17 -0800

    feat(components/layout): add AppHeader.vue

---
+++ components/layout/AppHeader.vue
@@ -0,0 +1,23 @@
+<template functional>
+  <div class="bg-white dark:bg-gray-900 dark:text-white h-screen">
+    <header class="flex flex-row">
+      <NuxtLink to="/" class="mr-24">
+        <h2>Chris Vaillancourt</h2>
+      </NuxtLink>
+      <nav>
+        <ul class="flex flex-row">
+          <NuxtLink to="/latest" class="mr-16">
+            <li>Latest</li>
+          </NuxtLink>
+          <NuxtLink to="/about" class="mr-16">
+            <li>About</li>
+          </NuxtLink>
+          <NuxtLink to="/projects" class="mr-16">
+            <li>Projects</li>
+          </NuxtLink>
+        </ul>
+      </nav>
+    </header>
+    <nuxt />
+  </div>
+</template>
