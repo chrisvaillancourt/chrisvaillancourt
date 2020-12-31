c9cb2bd8be8b05621e406980a348c473397f7c22
Chris Vaillancourt <cvaillancourt@denovium.com>
2020-12-31 13:45:11 -0800

    style(AppHeader): set header link color and decoration

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -1,17 +1,26 @@
 <template functional>
   <header class="flex flex-row dark:bg-gray-800 py-6 pl-8">
-    <NuxtLink to="/" class="mr-24">
+    <NuxtLink to="/" class="mr-24 dark:text-white dark:hover:text-green-500">
       <h2>Chris Vaillancourt</h2>
     </NuxtLink>
     <nav>
       <ul class="flex flex-row">
-        <NuxtLink to="/latest" class="mr-16">
+        <NuxtLink
+          to="/latest"
+          class="mr-16 dark:text-white dark:hover:text-green-500"
+        >
           <li>Latest</li>
         </NuxtLink>
-        <NuxtLink to="/about" class="mr-16">
+        <NuxtLink
+          to="/about"
+          class="mr-16 dark:text-white dark:hover:text-green-500"
+        >
           <li>About</li>
         </NuxtLink>
-        <NuxtLink to="/projects" class="mr-16">
+        <NuxtLink
+          to="/projects"
+          class="mr-16 dark:text-white dark:hover:text-green-500"
+        >
           <li>Projects</li>
         </NuxtLink>
       </ul>
