899f52fd770a75226f9e54edd28526e349066610
Chris Vaillancourt <cvaillancourt@denovium.com>
2020-12-31 15:37:37 -0800

    feat(AppHeader): add hamburger button on small displays

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -1,10 +1,12 @@
-<template functional>
-  <header class="flex flex-row dark:bg-gray-800 py-6 pl-8">
+<template>
+  <header
+    class="flex flex-row justify-between md:justify-start dark:bg-gray-800 py-6 pl-8"
+  >
     <NuxtLink to="/" class="mr-24 dark:text-white dark:hover:text-green-500">
       <h2>Chris Vaillancourt</h2>
     </NuxtLink>
     <nav>
-      <ul class="flex flex-row">
+      <ul class="hidden md:flex md:flex-row">
         <NuxtLink
           to="/latest"
           class="mr-16 dark:text-white dark:hover:text-green-500"
@@ -24,6 +26,23 @@
           <li>Projects</li>
         </NuxtLink>
       </ul>
+      <button class="md:hidden mr-8">
+        <AppIcon :icon="$options.static.icons.mdiMenu" />
+      </button>
     </nav>
   </header>
 </template>
+<script>
+import { mdiMenu } from '@mdi/js'
+
+export default {
+  name: 'AppHeader',
+  created() {
+    this.$options.static = {
+      icons: {
+        mdiMenu,
+      },
+    }
+  },
+}
+</script>
