2bffc7139d0e2824f212fd72c4fa3d9396587a03
Chris Vaillancourt <cvaillancourt@denovium.com>
2020-12-31 11:54:27 -0800

    refactor(AppHeader): remove div wrapper and add white space

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -1,22 +1,20 @@
 <template functional>
-  <div>
-    <header class="flex flex-row">
-      <NuxtLink to="/" class="mr-24">
-        <h2>Chris Vaillancourt</h2>
-      </NuxtLink>
-      <nav>
-        <ul class="flex flex-row">
-          <NuxtLink to="/latest" class="mr-16">
-            <li>Latest</li>
-          </NuxtLink>
-          <NuxtLink to="/about" class="mr-16">
-            <li>About</li>
-          </NuxtLink>
-          <NuxtLink to="/projects" class="mr-16">
-            <li>Projects</li>
-          </NuxtLink>
-        </ul>
-      </nav>
-    </header>
-  </div>
+  <header class="flex flex-row dark:bg-gray-800 py-6 pl-8">
+    <NuxtLink to="/" class="mr-24">
+      <h2>Chris Vaillancourt</h2>
+    </NuxtLink>
+    <nav>
+      <ul class="flex flex-row">
+        <NuxtLink to="/latest" class="mr-16">
+          <li>Latest</li>
+        </NuxtLink>
+        <NuxtLink to="/about" class="mr-16">
+          <li>About</li>
+        </NuxtLink>
+        <NuxtLink to="/projects" class="mr-16">
+          <li>Projects</li>
+        </NuxtLink>
+      </ul>
+    </nav>
+  </header>
 </template>
