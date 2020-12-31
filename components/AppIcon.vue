9c28033841cd30fbb8b3ccc73e9cb803dce647ef
Chris Vaillancourt <cvaillancourt@denovium.com>
2020-12-31 15:35:18 -0800

    feat: add AppIcon component

---
+++ components/AppIcon.vue
@@ -0,0 +1,37 @@
+<template functional>
+  <svg
+    :style="{ width: props.size, height: props.size }"
+    xmlns="http://www.w3.org/2000/svg"
+    :viewBox="props.viewBox"
+  >
+    <path :d="props.icon" :fill="props.color" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'AppIcon',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    size: {
+      type: String,
+      required: false,
+      default: '24px',
+    },
+    color: {
+      type: String,
+      required: false,
+      default: 'currentColor',
+    },
+    viewBox: {
+      type: String,
+      required: false,
+      default: '0 0 24 24',
+    },
+  },
+}
+</script>
