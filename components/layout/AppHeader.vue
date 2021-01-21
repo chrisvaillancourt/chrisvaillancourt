7470afc7d4face533c9c3124a92bd020a7f819a4
Chris Vaillancourt <cvaillan22@gmail.com>
2021-01-20 22:55:54 -0800

    style: temporarily hide the app header

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="flex flex-row justify-between md:justify-start dark:bg-gray-800 py-6 pl-8"
+    class="flex flex-row justify-between md:justify-start dark:bg-gray-800 py-6 pl-8 hidden"
   >
     <NuxtLink to="/" class="mr-24 dark:text-white dark:hover:text-green-500">
       <h2>Chris Vaillancourt</h2>
