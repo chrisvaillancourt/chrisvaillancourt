77a30de441bfca549b33d464f34f0831fdfa9117
Chris Vaillancourt <cvaillan22@gmail.com>
2021-01-30 12:00:48 -0800

    refactor: delete nuxt files

--- pages/about.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h2>About</h2>
-  </div>
-</template>
-
-<script>
-export default {}
-</script>
--- pages/projects.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h2>Projects stuff</h2>
-  </div>
-</template>
-
-<script>
-export default {}
-</script>
