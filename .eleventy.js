22085bc75e51dd0a1037e702768f433a52ce0416
Chris Vaillancourt <cvaillan22@gmail.com>
2021-02-02 08:04:30 -0800

    chore(eleventy config): remove custom file watcher & style copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,20 @@
-const htmlmin = require("html-minifier");
+const htmlmin = require('html-minifier');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
+  // instruct eleventy to rebuild when a file changes with eleventyConfig.addWatchTarget(file)
 
-  eleventyConfig.addWatchTarget("./_tmp/style.css");
+  // instruct eleventy to copy files or directories with eleventyConfig.addPassthroughCopy()
 
-  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
-
-  eleventyConfig.addShortcode("version", function () {
+  eleventyConfig.addShortcode('version', function () {
     return String(Date.now());
   });
 
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (
       process.env.ELEVENTY_PRODUCTION &&
       outputPath &&
-      outputPath.endsWith(".html")
+      outputPath.endsWith('.html')
     ) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
