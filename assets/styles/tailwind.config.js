f278ba4c052eed52060fec7696be898a0d2c07c9
Chris Vaillancourt <cvaillan22@gmail.com>
2021-02-18 09:13:01 -0800

    chore(tailwind.config.js): configure tailwindcss-pseudo-elements plugin

--- assets/styles/tailwind.config.js
+++ assets/styles/tailwind.config.js
@@ -1,17 +1,46 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   purge: {
-    content: ["_site/**/*.html"],
+    content: ['_site/**/*.html'],
     options: {
       safelist: [],
-    }
+    },
   },
   darkMode: 'media',
   theme: {
-    extend: {
-    },
-  },
-  variants: {
     extend: {},
   },
-  plugins: [],
-}
+  variants: {
+    extend: {
+      textColor: [
+        'responsive',
+        'hover',
+        'focus',
+        'before',
+        'after',
+        // If you want to combine it with a pseudo class,
+        // use `<pseudo-class>_<pseudo-element>`.
+        'hover_before',
+        'hover_after',
+        'focus_before',
+      ],
+    },
+  },
+  // Set up custom pseudo-classes and pseudo-elements.
+  customPseudoClasses: [],
+  customPseudoElements: [],
+  plugins: [
+    require('tailwindcss-pseudo-elements'),
+    plugin(function ({ addUtilities }) {
+      addUtilities(
+        {
+          '.empty-content': {
+            content: "''",
+          },
+        },
+        ['before']
+      );
+    }),
+  ],
+};
